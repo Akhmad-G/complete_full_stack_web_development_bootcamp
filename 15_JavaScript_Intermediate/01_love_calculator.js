94a262b3a27931e99c674eb4340035e5f22bea10
Akhmad Gazimuhammadilav <akhmad.gazimuhammadilav@outlook.com>
2026-04-22 13:01:56 +0200

    Lesson 15 start

---
+++ 15_JavaScript_Intermediate/01_love_calculator.js
@@ -0,0 +1,12 @@
+let mansName = prompt("What is man's name?").toLowerCase();
+let womanName = prompt("What is woman's name?").toLowerCase();
+
+function randomPercentage() {
+    return Math.floor(Math.random() * 100 + 1);
+}
+
+function nameCapitalaze (name) {
+    return name.slice(0, 1).toUpperCase() + name.slice(1, name.length)
+}
+
+alert(nameCapitalaze(mansName) + " is " + randomPercentage() + " percent compatible with " + nameCapitalaze(womanName) + ".")
