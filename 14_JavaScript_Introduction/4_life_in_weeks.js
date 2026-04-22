5df90e0077578894d2d6ccb9d3bd475d1ac43621
Akhmad Gazimuhammadilav <akhmad.gazimuhammadilav@outlook.com>
2026-04-22 12:40:41 +0200

    Lesson 14 is finished

---
+++ 14_JavaScript_Introduction/4_life_in_weeks.js
@@ -0,0 +1,13 @@
+function lifeInWeeks(age) {
+
+    /************Don't change the code above************/
+
+    let years = 90 - age;
+    let months = years * 12;
+    let weeks = years * 52;
+    let days = years * 365;
+
+    console.log("You have " + days + " days, " + weeks + " weeks, and " + months + " months left.");
+
+    /*************Don't change the code below**********/
+}
